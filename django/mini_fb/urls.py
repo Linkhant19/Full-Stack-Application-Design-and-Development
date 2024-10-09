71ece51fffbe69187c87abddb21cd030c75f0ecb
Linkhant19 <lkk19@bu.edu>
2024-10-08 23:39:48 -0400

    minor bug fixes

--- django/mini_fb/urls.py
+++ django/mini_fb/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 # create a list of URLs for the mini_fb app
 urlpatterns = [
-    path(r'', views.ShowAllProfilesView.as_view(), name='show_all'), # the first URL. main page
+    path(r'', views.ShowAllProfilesView.as_view(), name='show_all_fb'), # the first URL. main page
 ]
